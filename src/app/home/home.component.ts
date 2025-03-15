a58a9436be524c32010901f70147e94b130f760f
Scott <scotta.kinchlea@gmail.com>
2025-03-15 15:59:03 +0000

    fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, enableProdMode, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -10,5 +10,6 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    enableProdMode();
   }
 }
